a04bc76ae2d84ae4d0bc044165eec6e49213007b
Ketz4l <dtorresg2003@alumno.ipn.mx>
2022-03-03 23:24:01 -0600

    Examen

--- ExamenPractico/js/validacion.js
+++ ExamenPractico/js/validacion.js
@@ -1,13 +1,21 @@
 function validar(automovil){
     if(automovil.auto.value.length<100000)
-    alert("Es costo del automovil no es valido");
+    alert("Es costo del automovil no es válido");
         automovil.auto.focus();
         return false;
     if(automovil.auto.value.length>1000000)
-    alert("Es costo del automovil no es valido");
+    alert("Es costo del automovil no es válido");
         automovil.auto.focus();
         return false;
 }
+
+function validar(automovil){
+    if(automovil.cuoini.value <(10*auto)/100)
+    lert("El costo inicial no es válido");
+        automovil.cuoini.focus();
+        return false;
+}
+
 
 var checkOK = "1234567890";
 
@@ -15,6 +23,6 @@ var checkOK = "1234567890";
 
     var escorrecto = true;
 
-var suminteres = (cuota*3.94/100);
+var suminteres = (cuota*3.94)/100;
 
     var interes = cuota+suminteres;
