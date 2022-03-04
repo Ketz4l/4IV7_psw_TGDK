ba35306e74f501da874f27ff38bdacbc6264eb3f
Ketz4l <dtorresg2003@alumno.ipn.mx>
2022-03-03 23:05:04 -0600

    Examen

---
+++ ExamenPractico/js/validacion.js
@@ -0,0 +1,17 @@
+function validar(automovil){
+    if(automovil.auto.value.length<100000)
+    alert("Es costo del automovil no es valido");
+        automovil.auto.focus();
+        return false;
+    if(automovil.auto.value.length>1000000)
+    alert("Es costo del automovil no es valido");
+        automovil.auto.focus();
+        return false;
+}
+
+var checkOK = "1234567890";
+
+    var checkStr = automovil.auto.value;
+
+    var escorrecto = true;
+
